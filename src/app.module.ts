ecde16874e83d6f87eb8bc55ef7d3f33e4f9ffd0
MuniBarbieri <edmundobarbieri@gmail.com>
2025-04-30 12:20:58 -0300

    Update package with new dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseService } from './supabase.service';
+import { SupabaseModule } from './supabase.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TasksModule,
+    SupabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SupabaseService],
+  exports: [SupabaseService],
 })
 export class AppModule {}
+
