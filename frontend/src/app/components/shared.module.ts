601f4d8373766901dd9ad69b9ccb6294bede29af
MuniBarbieri <edmundobarbieri@gmail.com>
2025-05-05 00:51:51 -0300

    feat(front): Add skeleton edit dialog

--- frontend/src/app/components/shared.module.ts
+++ frontend/src/app/components/shared.module.ts
@@ -9,10 +9,11 @@ import { SkeletonTaskCardComponent } from './skeleton-task-card/skeleton-task-ca
 import { LoadingBarComponent } from './loading-bar/loading-bar.component';
 import { SkeletonTaskFormComponent } from './skeleton-task-form/skeleton-task-form.component';
 import { PriorityBadgeComponent } from './priority-badge/priority-badge.component';
+import { SkeletonEditDialogComponent } from './skeleton-edit-dialog/skeleton-edit-dialog.component';
 
 @NgModule({
-  declarations: [AddTaskFormComponent, TaskSectionComponent, TaskCardComponent, SkeletonTaskCardComponent, LoadingBarComponent, SkeletonTaskFormComponent, PriorityBadgeComponent],
+  declarations: [AddTaskFormComponent, TaskSectionComponent, TaskCardComponent, SkeletonTaskCardComponent, LoadingBarComponent, SkeletonTaskFormComponent, PriorityBadgeComponent, SkeletonEditDialogComponent],
   imports: [CommonModule, ReactiveFormsModule, LucideAngularModule.pick(icons)],
-  exports: [AddTaskFormComponent, TaskSectionComponent, TaskCardComponent,LucideAngularModule, SkeletonTaskCardComponent, LoadingBarComponent, SkeletonTaskFormComponent, PriorityBadgeComponent],
+  exports: [AddTaskFormComponent, TaskSectionComponent, TaskCardComponent,LucideAngularModule, SkeletonTaskCardComponent, LoadingBarComponent, SkeletonTaskFormComponent, PriorityBadgeComponent, SkeletonEditDialogComponent],
 })
 export class SharedModule {}
---
+++ frontend/src/app/components/skeleton-edit-dialog/skeleton-edit-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-edit-dialog',
+  standalone: false,
+  templateUrl: './skeleton-edit-dialog.component.html',
+  styleUrl: './skeleton-edit-dialog.component.scss'
+})
+export class SkeletonEditDialogComponent {
+
+}
